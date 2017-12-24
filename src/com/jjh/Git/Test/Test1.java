70e587b11f711e2274f0c64e5efc47e27e4db70c
ryy <962324599@qq.com>
2017-12-24 14:51:31 +0800

    第三次

--- src/com/jjh/Git/Test/Test1.java
+++ src/com/jjh/Git/Test/Test1.java
@@ -2,8 +2,8 @@ package com.jjh.Git.Test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("张飞在用荆轲打张飞！");
-		System.out.println("荆轲在用张飞打荆轲！");
+		System.out.println("张飞在用荆轲打张飞！6");
+		System.out.println("荆轲在用张飞打荆轲！66");
 		
 	}
 
